a698da7a2776369654b1b883cb08bc0a67476700
José Neilson <joseneilson13@gmail.com>
2025-10-16 09:02:39 -0300

    add test to welcome page

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  fixtures :users
+  include Devise::Test::IntegrationHelpers
+
+  test "should redirect to login" do
+    get root_url
+    assert_redirected_to new_user_session_url
+  end
+
+  test "authenticated users can see index" do
+    sign_in users(:one)
+
     get root_url
     assert_response :success
   end
